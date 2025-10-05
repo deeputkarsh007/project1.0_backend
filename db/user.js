ccf817b2f36c769038d6f37437eda2314b3ca98f
deeputkarsh007 <deeputkarsh007@gmail.com>
2025-10-05 20:32:44 +0530

    payment

--- db/user.js
+++ db/user.js
@@ -4,5 +4,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  coin: { type: Number, default: 25 }, // Corrected 'coint' to 'coin', and changed to Number
 });
-module.exports = mongoose.model("users", userSchema);
+
+module.exports = mongoose.model("User", userSchema);
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 const auth = require("./routes/auth");
 const people = require("./routes/user");
